abfde83a0671a65aa29eb081bf722ac67bea09ce
Giovanne Cabriotti Danna <giovannecabriottidanna@gmail.com>
2024-11-13 15:50:17 -0300

    Desafio React 3 Completo V1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Feed } from "./pages/feed";
 import { Home } from './pages/home'
 import { Login } from './pages/login'
 import { GlobalStyle } from './styles/global';
+import { Sign } from './pages/sign';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/feed" element={<Feed />} />
+        <Route path="/sign" element={<Sign/>} />
      </Routes >
     </Router>
   );
